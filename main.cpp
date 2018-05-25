ad765d546c5464b1ed594fe96e8db90c7b2e346e
Willian Talles Marcolino Dantas <willian.talles@A309-21L.imd.local>
2018-05-24 22:10:21 -0300

    inicio

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <set>
+using std::set;
+
+template<typename TContainer>
+void print_elements(const TContainer& collection, const char* label="", const char separator=' '){
+	
+	for (auto i = collection.begin(); i != collection.end() ; ++i){
+		cout << label << (*i) << separator;
+	}
+
+	cout<<endl;
+}
+
+int main() {
+	set<int> numeros;
+	numeros.insert(3);
+	numeros.insert(1);
+	numeros.insert(4);
+	numeros.insert(1);
+	numeros.insert(2);
+	numeros.insert(5);
+	print_elements(numeros, "Set: ");
+	print_elements(numeros, "CSV Set: ", ';');
+	return 0;
+}
